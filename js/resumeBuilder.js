55ad03ead3716c41f88b001bde146ccc7331a4e3
Matias M <matiasmasca@gmail.com>
2015-01-03 13:30:42 -0300

    Termianando modulo 1. 4 objetos, interfaz limpia.

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,30 +1,47 @@
-var name = 	"Matías Mascazzini";
-var role = 	"Tecnólogo";
-var age = 33;
-var contact_info = {
-	"mobile":"379-4781004",
-	"email":"matiasmasca@gmail.com",
-	"github":"matiasmasca",
-	"twitter":"@matiasmasca",
-	"location":"Corrientes"
-};
-var skills = ["Desarrollador Web","Emprendedor"];
-
 var bio = {
-	"name" : name,
-	"role" : role,
-	"age" : age,
-	"contactInfo" : contact_info ,
-	"bioPictureURL" : "images/me.jpg",
+	"name" : "Matías Mascazzini",
+	"role" : "Tecnólogo",
 	"welcomeMessage" : "Hola Mundo JavaScript",
-	"skills": skills
+	"age" : "33",
+	"contactInfo" : {
+		"mobile":"379-4781004",
+		"email":"matiasmasca@gmail.com",
+		"github":"matiasmasca",
+		"twitter":"@matiasmasca",
+		"location":"Corrientes, Corrientes, ARG"
+	},
+	"bioPictureURL" : "images/me.jpg",
+	
+	"skills": ["Desarrollador Web","Emprendedor", "Generalista"]
 };
 
-var work = {};
-work.currentJobPosition = "Consultor Tecnólogico";
-work.employer = "TEX, Terciar and Connextion";
-work.years = "2000-2014";
-work.city = "Corrientes, ARG";
+var work = {
+	"jobs": [
+		{"employer":"", "title":"", "location":"", "datesWorked":"", "description":""},
+		{"employer":"", "title":"", "location":"", "datesWorked":"", "description":""}
+	],
+	"currentJobPosition": "Consultor Tecnólogico",
+	"employer": "TEX, Terciar and Connextion",
+	"years": "2014",
+	"city" : "Corrientes, ARG"
+};
+
+ Each project object in projects should contain a title, dates worked, description, and an images array with URL strings for project images.
+var projects = { "projects": 
+	[{
+		"title":" Proyecto 1", 
+		"datesWorked":"", 
+		"description":"", 
+		"images" : ["url1","url2"] 
+		},
+		{
+		"title":"Proyecto 2", 
+		"datesWorked":"", 
+		"description":"", 
+		"images" : ["url1","url2"] 
+		}
+	]
+};
 
 var education = {
 "schools" : [
@@ -32,7 +49,7 @@ var education = {
 	"name": "Facultad de Ciencias Exactas (Universidad Nacional de Nordeste)",
 	"city" : "Corrientes, Corrientes, ARG", 
 	"year" : "2008",
-	"degree": "pre-grado"
+	"degree": "pre-grado",
 	"minor" : "CompSci",
 	"title" : "Programador Universitario de Aplicaciones."
   },
@@ -50,23 +67,14 @@ var education = {
 	{
 		"title": "SaaS",
 		"school":"UC BerkeleyX",
-		"dates": 2014
+		"dates": 2014,
+		"url": "url_al_curso"
 	},
 	{
 		"title": "JavaScript Basics",
 		"school":"Udacity",
-		"dates": 2014
+		"dates": 2015,
+		"url": "url_al_curso"
 	}
 	]
 };
-
-
-var formattedName = HTMLheaderName.replace("%data%",bio.name);
-var formattedRole = HTMLheaderRole.replace("%data%",bio.role);
-
-$("#main").prepend(formattedRole);
-$("#main").prepend(formattedName);
-$("#main").append(bio.contactInfo.email);
-
-$("#main").append(work["currentJobPosition"]);
-$("#main").append(education.lastSchool);
